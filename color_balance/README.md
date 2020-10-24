e8a61772e4429a54dabe347fe03524898846fdae
vincentgit <vincentcheng929@gmail.com>
2020-10-24 10:09:05 +0800

    Add halide opencv benchmark

---
+++ color_balance/README.md
@@ -0,0 +1,3 @@
+Auto schedule: 0.311628ms
+Manual schedule: 0.266352ms
+Opencv: 0.54312ms
--- color_balance/color_balance_halide_generator.cpp
+++ color_balance/color_balance_halide_generator.cpp
@@ -11,16 +11,21 @@ public:
 	Halide::Var y;
 	Halide::Var c;
 
+	Halide::Func hist;
+	Halide::Func cdf;
+	Halide::Func min_count;
+	Halide::Func max_count;
+	Halide::Func min_sum;
+	Halide::Func max_sum;
+
 	void generate() {
 		// 1. calculate histogram
-		Halide::Func hist("hist");
 		hist(x, c) = 0.0f;
 		Halide::RDom r(0, input.width(), 0, input.height());
 		Halide::Expr value = Halide::cast<uint8_t>(Halide::clamp(input(r.x, r.y, c)*255.0f, 0.0f, 255.0f));
 		hist(value, c) += 1.0f;
 
 		// 2. calculate cumulative distribute function
-		Halide::Func cdf("cdf");
 		cdf(x, c) = 0.0f;
 		Halide::RDom rcdf(0, 256);
 		cdf(rcdf.x+1, c) = cdf(rcdf.x, c) + hist(rcdf.x, c); 
@@ -30,11 +35,6 @@ public:
 		Halide::Expr num_pixels = input.width() * input.height();
 		Halide::Expr min_threshold = num_pixels * s / 2;
 		Halide::Expr max_threshold = num_pixels * (1.0f - s / 2);
-
-		Halide::Func min_count("min_count");
-		Halide::Func max_count("max_count");
-		Halide::Func min_sum("min_sum");
-		Halide::Func max_sum("max_sum");
 
 		min_count(x, c) = 0.0f;
 		min_count(x, c) = Halide::select(cdf(x, c) < min_threshold, 1.0f, 0.0f);
@@ -48,15 +48,20 @@ public:
 		max_sum(c) = Halide::sum(max_count(rcdf.x, c));
 
 		// 4. adjust the ove dark and over light pixels
-		// Halide::Func color_balance("color_balance");
-		// color_balance(x, y, c) = 0.0f;
 		output(x, y, c) = Halide::clamp(input(x, y, c), min_sum(c)/255.0f, max_sum(c)/255.0f);
 	}
 
 	void schedule() {
 		if (auto_schedule) {
-			input.set_estimates({{0, 1024}, {0, 1024}, {0, 3}});
-			output.set_estimates({{0, 1024}, {0, 1024}, {0, 3}});
+			input.set_estimates({{0, 2048}, {0, 2048}, {0, 3}});
+			output.set_estimates({{0, 2048}, {0, 2048}, {0, 3}});
+		} else {
+			hist.compute_root();
+			cdf.compute_root();
+			min_count.compute_root();
+			max_count.compute_root();
+			min_sum.compute_root();
+			max_sum.compute_root();
 		}
 	}
 };
