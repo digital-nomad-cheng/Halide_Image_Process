6feadedd980f11f7cefaf8d4ae140aadd519b3f2
vincent <vincentcheng929@gmail.com>
2020-10-26 11:03:04 +0800

    Add rgb2bgr

---
+++ rgb2bgr/compile_link.sh
@@ -0,0 +1,2 @@
+# halide jit
+g++ -std=c++11 rgb2bgr_jit.cpp -L /usr/local/lib/ -I /Users/vincent/Libs/Halide/tools -lHalide -ljpeg -lpng -o rgb2bgr_jit
---
+++ rgb2bgr/rgb2bgr_jit.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <Halide.h>
+#include <halide_image_io.h>
+
+Halide::Func rgb2bgr(Halide::Buffer<uint8_t> &src) 
+{
+	Halide::Var x;
+	Halide::Var y;
+	Halide::Var c;
+
+	Halide::Func dst;
+
+	dst(x, y, c) = Halide::select(c == 0, src(x, y, 2), c == 1, src(x, y, 1), src(x, y, 2));
+
+    return dst;
+
+}
+
+int main(int argc, char ** argv)
+{
+	Halide::Buffer<uint8_t> src = Halide::Tools::load_image("../images/Lenna.png");
+	Halide::Buffer<uint8_t> dst = rgb2bgr(src).realize(src.width(), src.height(), src.channels());
+	Halide::Tools::save_image(dst, "./results/Lenna_bgr.png");
+}
